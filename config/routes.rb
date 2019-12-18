dca4fd5cd86737fa531e9a37a0ffb8f1dc4b7e1e
David Elì <davidelimg1@gmail.com>
2019-12-18 12:21:41 -0600

    added login service

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   get 'users/new'
-  resources :users
   get 'sessions/new'
   get '/signup', to: 'users#new'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
