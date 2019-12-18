876935fde6a7f669c9b3429fbb621f3821135951
David Elì <davidelimg1@gmail.com>
2019-12-18 09:42:06 -0600

    Added has_secure_password to user model

--- Gemfile
+++ Gemfile
@@ -5,6 +5,8 @@ ruby '2.6.4'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.4'
+
+gem 'bcrypt', '~> 3.1.11'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
 # Use Puma as the app server
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  validates :name,  presence: true, length: { maximum: 50 }
+  validates :email, presence: true, length: { maximum: 255 },
+                      format: { with: URI::MailTo::EMAIL_REGEXP },
+                      uniqueness: { case_sensitive: false }
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
 end
