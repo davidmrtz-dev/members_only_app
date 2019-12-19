40e54b2c852f44e9183c63dcb3dc4c6a75f1375d
David Elì <davidelimg1@gmail.com>
2019-12-19 11:41:47 -0600

    Added login service to acces to posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :valid_user, only: %i[new create]
+  before_action :logged_in_user, only: %i[new create]
   def new
     @post = Post.new
   end
@@ -12,6 +12,17 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      log_in @post
+      flash[:success] = 'Welcome to the Sample App!'
+      redirect_to @post
+    else
+      render 'new'
+    end
+  end
+
   def post_params
     params.require(:post).permit(:content, :user_id)
   end
@@ -19,9 +30,11 @@ class PostsController < ApplicationController
   private
 
   # Confirms a valid user.
-  def valid_user
-    unless @user #&& @user.authenticate(params[:password])
-      redirect_to root_url
-    end
+  def logged_in_user
+    return if logged_in?
+
+    store_location
+    flash[:danger] = 'Please log in'
+    redirect_to login_url
   end
 end
