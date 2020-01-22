a69a754748eb3a932893335f8743e0b311983770
David Elì <davidelimg1@gmail.com>
2020-01-21 23:34:25 -0600

    Basic login full implemented

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,20 +9,11 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find_by(id: session[:user_id])
   end
 
-  def current_user=(user)
-    if user
-      session[:user_id] = user.id
-    else
-      session.delete :user_id
-    end
-    @current_user = user || nil
-  end
-
   def logged_in_user
-    return if logged_in?
-
+    unless logged_in?
     flash[:danger] = 'Please log in'
     redirect_to login_url
+    end
   end
 
   def logged_in?
