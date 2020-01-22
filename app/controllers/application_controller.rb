3e48fa0c90085c105c23b997048753a2f45d67c7
David Elì <davidelimg1@gmail.com>
2020-01-21 22:40:51 -0600

    Add navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  #helper_method :current_user
+  helper_method :current_user
   helper_method :logged_in?
   protect_from_forgery with: :exception
 
