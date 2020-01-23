24f2e5318e81c0412331670c6d35994706daf75a
David Elì <davidelimg1@gmail.com>
2020-01-23 01:49:20 -0600

    Restric access to edit user only for signed in users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: %i[show]
+  before_action :logged_in_user, only: %i[show edit update]
 
   def show
     @user = User.find(params[:id])
