73ce0afa0d26ae75600e77c5f26bac3d2388b566
David Elì <davidelimg1@gmail.com>
2020-01-25 22:44:27 -0600

    Edited view to display delete post only to admin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: %i[index show edit update destroy]
   before_action :correct_user, only: %i[edit update]
-  before_action :admin_user, only: :destroy
+  before_action :admin_user, only: %i[destroy]
 
   def index
     @users = User.all
