a6037ddfd46e1a1cba8a5a30965d5144e9d81741
David Elì <davidelimg1@gmail.com>
2020-01-31 23:54:45 -0600

    Fixed some rubocop issues

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -16,6 +16,6 @@ class UserMailer < ApplicationMailer
   #
   def password_reset(user)
     @user = user
-    mail to: user.email, subject: "Password reset"
+    mail to: user.email, subject: 'Password reset'
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -67,7 +67,7 @@ class User < ApplicationRecord
     UserMailer.password_reset(self).deliver_now
   end
 
-  #returns true if a password reset has expired.
+  # Returns true if a password reset has expired.
   def password_reset_expired?
     reset_sent_at < 2.hours.ago
   end
