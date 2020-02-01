61a5f762a2a5cd693f257a6db27848f375c54851
David Elì <davidelimg1@gmail.com>
2020-02-01 00:50:35 -0600

    Edited posts views for the owner of the posts and all the posts view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,7 +8,7 @@ class PostsController < ApplicationController
 
   def index
     @posts = Post.all
-    @posts = Post.paginate(page: params[:page], per_page: 20)
+    @posts = Post.paginate(page: params[:page], per_page: 10)
   end
 
   def show
