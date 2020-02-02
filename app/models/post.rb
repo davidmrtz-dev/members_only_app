ff51d8a35d299862f6d736ed8844509bd09b039f
David Elì <davidelimg1@gmail.com>
2020-02-02 00:15:33 -0600

    Edited homew view, session redirect to home, and removed limit for posts chars

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
   default_scope -> { order(created_at: :desc) }
-  validates :content, presence: true, length: { maximum: 200 }
+  validates :content, presence: true
   validates :user_id, presence: true
 end
