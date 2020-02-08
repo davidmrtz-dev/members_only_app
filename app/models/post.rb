2a1f0cdc9785e7d597428d5556e0f5ca0ef2b799
David Elì <davidelimg1@gmail.com>
2020-02-07 22:24:42 -0600

    Fixed more lint issues

--- app/models/post.rb
+++ app/models/post.rb
@@ -11,6 +11,7 @@ class Post < ApplicationRecord
   # Validates the size of an uploaded picture.
   def picture_size
     return unless picture.size > 5.megabytes
+
     errors.add(:picture, 'should be less than 5MB')
   end
 end
