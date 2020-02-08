a227ff8f84e67a4345f733b30e8686b8b4a9173f
David Elì <davidelimg1@gmail.com>
2020-02-07 22:21:31 -0600

    Fixed some linter issues

--- app/models/post.rb
+++ app/models/post.rb
@@ -11,7 +11,6 @@ class Post < ApplicationRecord
   # Validates the size of an uploaded picture.
   def picture_size
     return unless picture.size > 5.megabytes
-    
     errors.add(:picture, 'should be less than 5MB')
   end
 end
