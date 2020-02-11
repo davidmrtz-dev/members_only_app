d76eb3b26a75801e38a286a430075c95fec11734
David Elì <davidelimg1@gmail.com>
2020-02-11 14:15:21 -0600

    Added relationship to users model to get the followers

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,13 @@
 class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :active_relationships, class_name: 'Relationship',
-  foreign_key: 'follower_id', dependent: :destroy
-  has_many :active_relationships, through: :active_relationships,
-  source: :followed
-
+                                  foreign_key: 'follower_id', 
+                                  dependent: :destroy
+  has_many :passive_relationships, class_name: 'Relationship',
+                                  foreign_key: 'followed_id',
+                                  dependent: :destroy 
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
   # Create an accessible attribute.
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save :downcase_email
