bb8a16b2cde56007c7fab133e45f367b3d577a5f
David Elì <davidelimg1@gmail.com>
2020-02-11 11:44:42 -0600

    Added relations on user and relathionship models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ApplicationRecord
   has_many :posts, dependent: :destroy
+  has_many :active_relationships, class_name: 'Relationship',
+  foreign_key: 'follower_id', dependent: :destroy
+  has_many :active_relationships, through: :active_relationships,
+  source: :followed
+
   # Create an accessible attribute.
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save :downcase_email
