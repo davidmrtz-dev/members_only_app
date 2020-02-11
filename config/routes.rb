0c6463da452d32ad9a773501d4f0aa984b5affb2
David Elì <davidelimg1@gmail.com>
2020-02-11 14:31:40 -0600

    Added some moc data, and routes for following and followers pages for each user

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,11 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :posts
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
--- db/seeds.rb
+++ db/seeds.rb
@@ -33,3 +33,11 @@ users = User.all
                         user_id: user.id)
   end
 end
+
+# Following relationships
+users = User.all
+user  = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
