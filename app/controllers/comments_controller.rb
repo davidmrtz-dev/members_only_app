475b335b04662eb0a24052895f4859d78c9d9808
David Elì <davidelimg1@gmail.com>
2020-02-13 17:45:16 -0600

    Added featur e to insert comments on posts

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,20 @@
 class CommentsController < ApplicationController
-  def index
+  before_action :logged_in_user
+
+  def create
+    @comment = Comment.new(comment_params)
+    @comment.user_id = current_user.id
+    if @comment.save
+      redirect_to @comment.post
+    else
+      flash.now[:danger] = 'error'
+      redirect_back(fallback_location: root_path)
+    end
+  end
+  
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content, :user_id, :post_id)
   end
 end
